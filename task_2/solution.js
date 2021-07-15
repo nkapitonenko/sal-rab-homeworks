57efb146b489651f67ad43ffe13c298a19dcb55a
Natalia Ianushevich <natakapitonenko@yandex.ru>
2021-07-15 23:25:49 +0300

    Выполнено задание task_2

--- task_2/solution.js
+++ task_2/solution.js
@@ -1,7 +1,7 @@
 function calcShipping(sum, min, shipping) {
     let productSum = sum; // сумма в корзине
-    let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
-    let shippingPrice = shipping; // стоимость доставки
+    let freeShippingMinSum = 701; // минимальная цена для бесплатной доставки
+    let shippingPrice = 200; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
 
@@ -26,8 +26,8 @@ function calcShipping(sum, min, shipping) {
 
 function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
-    let discountMinSum = min; // минимальная цена для скидки
-    let discountPart = discount; // величина скидки в процентах
+    let discountMinSum = 700; // минимальная цена для скидки
+    let discountPart = 10; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
 
