52c0ed1c5a3871e1e585cc3fec4b3f98a4bb0833
Natalia Ianushevich <natakapitonenko@yandex.ru>
2021-07-16 12:23:56 +0300

    Выполнено задание task_2

--- task_2/solution.js
+++ task_2/solution.js
@@ -1,7 +1,7 @@
-function calcShipping(sum, freeShippingMinSum, shippingPrice) {
+function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
-    let freeShippingMinSum = 701; // минимальная цена для бесплатной доставки
-    let shippingPrice = 200; // стоимость доставки
+    let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
+    let shippingPrice = shipping; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
 
@@ -24,10 +24,10 @@ function calcShipping(sum, freeShippingMinSum, shippingPrice) {
     return shippingSum;
 }
 
-function calcDiscount(sum, discountMinSum, discountPart) {
+function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
-    let discountMinSum = 700; // минимальная цена для скидки
-    let discountPart = 10; // величина скидки в процентах
+    let discountMinSum = min; // минимальная цена для скидки
+    let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
 
@@ -61,7 +61,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     let freeShipping; // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
-    freeShipping = (shippingSum = 0) ? true : false; // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    freeShipping = (shippingSum === 0) ? true : false; // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 
     // Конец решения задачи №2.3.
 
